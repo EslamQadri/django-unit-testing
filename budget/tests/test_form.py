102b0e36c0475cbcc2e4a768cfce31a4565714d7
Eslam Qadri <eslamkadryuwk@gmail.com>
2022-07-06 18:33:49 +0200

    forms  tested

--- budget/tests/test_form.py
+++ budget/tests/test_form.py
@@ -1,1 +1,17 @@
+from django.test import TestCase
+from budget.forms import ExpenseForm
+class TestForms(TestCase):
+    #test 12
+    def test_ExpenseForm_valed_date(self):
+        form=ExpenseForm(data={
+            'title':'any thing',
+            'amount':1000,
+            'category':'any category'
 
+        })
+        self.assertTrue(form.is_valid())
+        #test 13
+    def test_ExpenseForm_no_data(self):
+        form=ExpenseForm(data={})
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors),3)
